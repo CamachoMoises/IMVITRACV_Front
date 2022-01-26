d62c27268849692b517788b7dd7a96a1af6a9a3c
moises2702 <moisescamacho26@gmail.com>
2022-01-26 15:10:52 -0400

    change theme of application

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -14,13 +14,13 @@ export class ConfigService {
   setConfigData() {
     this.configData = {
       layout: {
-        rtl: false, // options:  true & false
-        variant: 'dark', // options:  light & dark
-        theme_color: 'black', // options:  white, black, purple, blue, cyan, green, orange
-        logo_bg_color: 'black', // options:  white, black, purple, blue, cyan, green, orange
+        rtl: true, // options:  true & false
+        variant: 'light', // options:  light & dark
+        theme_color: 'white', // options:  white, black, purple, blue, cyan, green, orange
+        logo_bg_color: 'white', // options:  white, black, purple, blue, cyan, green, orange
         sidebar: {
           collapsed: true, // options:  true & false
-          backgroundColor: 'dark' // options:  light & dark
+          backgroundColor: 'light' // options:  light & dark
         }
       }
     };
